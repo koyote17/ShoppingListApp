ee466eedd052717b0f66d779c2069aaaa423748c
koyote17 <kacper.zab.kol@gmail.com>
2024-12-02 02:04:11 +0100

    Upload project

---
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.shoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShoppingListAppTheme {
+                Surface(modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background) {
+                }
+            }
+                ShoppingListApp()
+        }
+    }
+}
