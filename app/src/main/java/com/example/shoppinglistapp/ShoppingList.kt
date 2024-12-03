a0a4ded94144ab8cccf954d3dbf9d8a49b72d828
koyote17 <kacper.zab.kol@gmail.com>
2024-12-04 00:15:53 +0100

    ShoppingListItem function added

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -1,14 +1,26 @@
 package com.example.shoppinglistapp
 
+import android.content.ClipData.Item
+import android.graphics.drawable.shapes.Shape
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,7 +31,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 
 data class ShoppingItem(val id: Int,
@@ -46,14 +61,36 @@ fun ShoppingListApp(){
             .padding(16.dp),
         ) {
             items(sItems){
-
+                ShoppingListItem(it, {}, {})
             }
         }
     }
     if (showDialog){
         AlertDialog(onDismissRequest = {showDialog = false},
-            confirmButton = {},
+            confirmButton = {
+                Row(modifier = Modifier.fillMaxWidth().padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    Button(onClick = {
+                        if(itemName.isNotBlank()){
+                            val newItem = ShoppingItem(
+                                id = sItems.size+1,
+                                name = itemName,
+                                quantity = itemQuantity.toInt()
+                            )
+                            sItems = sItems + newItem
+                            showDialog = false
+                            itemName = ""
+                        }
+                    }) {
+                        Text("ADD")
+                    }
+                    Button(onClick = {showDialog = false}) {
+                        Text("CANCEL")
+                    }
+                }
+            },
             title = { Text("Add Shopping Item")},
+            containerColor = Color.LightGray,
             text = {
                 Column {
                     OutlinedTextField(
@@ -67,10 +104,37 @@ fun ShoppingListApp(){
                         value = itemQuantity,
                         onValueChange = { itemQuantity = it },
                         singleLine = true,
-                        modifier = Modifier.fillMaxWidth().padding(16.dp)
+                        modifier = Modifier.fillMaxWidth().padding(16.dp),
+
+                      /*  IconButton(onClick = {itemQuantity+1}) {
+                            Icon(imageVector = Icons.Default.Add,
+                                contentDescription = "") } */
                     )
                 }
             }
         )
     }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    isEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+){
+    Row(modifier = Modifier.fillMaxWidth().padding(8.dp).border(
+        border = BorderStroke(2.dp, Color.Green ),
+        shape = RoundedCornerShape(25)
+    )) {
+        Text(item.name, modifier = Modifier.padding(8.dp),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold)
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShoppingListAppPreview() {
+    ShoppingListApp()
 }
